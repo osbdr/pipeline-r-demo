c7c62a7af1408b42ced5893ff4c5ed356c89d21e
Jonas Michaelis <jonas.michaelis@bdr.de>
2020-06-08 09:07:52 +0200

    init

---
+++ ui.R
@@ -0,0 +1,16 @@
+library(shinydashboard)
+
+dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(),
+  dashboardBody(
+    fluidRow(
+      box(plotOutput("plot1", height = 250)),
+
+      box(
+        title = "Controls",
+        sliderInput("slider", "Number of observations:", 1, 100, 50)
+      )
+    )
+  )
+)
